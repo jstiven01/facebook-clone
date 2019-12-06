1e6498068fe880170e8fb9929b5203cc0da94e64
jstiven01 <stiven10@gmail.com>
2019-12-06 08:48:38 -0500

    Showing friends in User's Profile and refactoring user partial

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,18 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.full_name, class: 'gravatar')
   end
+
+  def friendship_requested(user)
+    friendship_requested = current_user.friendships.filter { |friendship| friendship.friend_id == user.id }
+    if friendship_requested.empty?
+      friendship_requested = user.friendships.filter { |friendship| friendship.friend_id == current_user.id }
+    end
+    friendship_requested
+  end
+
+  def delete_friend_link(user)
+    return unless current_page?(friendships_path)
+
+    link_to 'Delete', friendship_requested(user), method: :delete, data: { confirm: 'You sure?' }
+  end
 end
